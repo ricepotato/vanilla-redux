c6c19a49bd577579a435ee913d62c1b286f4eacf
Sukjun Sagong <ricepotato40@gmail.com>
2020-04-27 19:21:11 +0900

    vanilla redux start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const plus = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+let count = 0;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const updateText = () => {
+  number.innerHTML = count;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const handleAdd = () => {
+  count += 1;
+  updateText();
+};
+
+const handleMinus = () => {
+  count -= 1;
+  updateText();
+};
+
+plus.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
