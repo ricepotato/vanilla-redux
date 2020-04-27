8a32611f70385d1451e228d06ccfa2db5ada81b4
Sukjun Sagong <ricepotato40@gmail.com>
2020-04-27 19:38:23 +0900

    redux state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
-let count = 0;
 
-const updateText = () => {
-  number.innerHTML = count;
+const countModifier = (count = 0, action) => {
+  //console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
-
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
-
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+console.log(countStore.getState());
